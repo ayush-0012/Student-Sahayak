52b2314b9ed85cbb4a85fe150a7f9dbee7f73ba7
ayush-0012 <ayushbisht0012@gmail.com>
2025-07-27 21:21:26 +0530

    minor changes

--- src/components/sections/Video.tsx
+++ src/components/sections/Video.tsx
@@ -25,7 +25,7 @@ export default function VideoSection() {
               <iframe
                 width="100%"
                 height="100%"
-                src="https://www.youtube.com/embed/tSD-Y2VsmII"
+                src="https://youtube.com/watch?v=AuEMN3gE-Fo&feature=shared"
                 title="CRACK ANY EXAM WITH US| DAILY TARGET TRACKING| BUILD CONSISTENCY WITH US."
                 frameBorder="0"
                 allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
