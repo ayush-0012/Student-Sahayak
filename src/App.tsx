d7a536457bbedb883adec3eaaf47e06ac56d49eb
ayush-0012 <ayushbisht0012@gmail.com>
2025-07-31 11:17:39 +0530

    minor changes in routing and components

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,8 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Landing from "./components/Landing.js";
 import LoginForm from "./components/Login.js";
 import RegisterForm from "./components/Register.js";
+import VerifyEmailPage from "./components/Verification.js";
+import Dashboard from "./components/Dashboard.js";
 
 function App() {
   return (
@@ -11,6 +13,8 @@ function App() {
           <Route path="/" element={<Landing />} />
           <Route path="/login" element={<LoginForm />} />
           <Route path="/register" element={<RegisterForm />} />
+          <Route path="/verify-email" element={<VerifyEmailPage />} />
+          <Route path="/dashboard" element={<Dashboard />} />
         </Routes>
       </BrowserRouter>
     </>
