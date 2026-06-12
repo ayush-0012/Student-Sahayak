b3df2455957e92b491709c0564ccd18e62db46ca
ayush-0012 <ayushbisht0012@gmail.com>
2026-06-12 23:09:36 +0530

    commented leaderboard for now

--- src/components/Landing.tsx
+++ src/components/Landing.tsx
@@ -1,7 +1,7 @@
 import { ToastContainer } from "react-toastify";
 import Contact from "./sections/Contact";
 import Footer from "./sections/Footer";
-import HallOfFame from "./sections/HallOfFame";
+// import HallOfFame from "./sections/HallOfFame";
 import Hero from "./sections/Hero";
 import Mission from "./sections/Mission";
 import VideoSection from "./sections/Video";
@@ -13,7 +13,7 @@ export default function StudentSahayakLanding() {
       <Hero />
 
       {/* 1% Outcompetitors Wall */}
-      <HallOfFame />
+      {/* <HallOfFame /> */}
 
       {/* Stats Section (we can use this in future)*/}
       {/* <section className="bg-gray-900 py-16">
